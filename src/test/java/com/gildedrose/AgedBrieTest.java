57c664d3641eafe52ed139d423790e6f6566fac2
sandervangolen <sander.vangolen@sannister.be>
2025-01-19 14:52:49 +0100

    Added AgedBrie tests.

---
+++ src/test/java/com/gildedrose/AgedBrieTest.java
@@ -0,0 +1,33 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AgedBrieTest {
+
+    @Test
+    public void qualityIncreases() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item agedBrie = app.items[0];
+        assertEquals("Aged Brie", agedBrie.name);
+        assertEquals(21, agedBrie.quality);
+        assertEquals(9, agedBrie.sellIn);
+    }
+
+    @Test
+    public void qualityNeverGoesOver50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 50) };
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 60; i++) {
+            app.updateQuality();
+        }
+
+        assertEquals(50, app.items[0].quality);
+        assertEquals(-50, app.items[0].sellIn);
+    }
+}
