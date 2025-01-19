6286bbc89f7c1c8cf0d453804c8e4fe16f7ed72e
sandervangolen <sander.vangolen@sannister.be>
2025-01-19 15:08:32 +0100

    Added BackstagePasses tests.

---
+++ src/test/java/com/gildedrose/BackstagePassesTest.java
@@ -0,0 +1,45 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BackstagePassesTest {
+
+    @Test
+    public void qualityIncreaseVariesBySellin() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        // quality increases by 1
+        assertEquals(10, items[0].sellIn);
+        assertEquals(21, items[0].quality);
+
+        app.updateQuality();
+        // quality increases by 2
+        assertEquals(9, items[0].sellIn);
+        assertEquals(23, items[0].quality);
+
+        for( int i = 0; i < 5; i++) {
+            app.updateQuality();
+        }
+
+        // quality increases by 3
+        assertEquals(4, items[0].sellIn);
+        assertEquals(34, items[0].quality);
+    }
+
+    @Test
+    public void qualityGoesToZeroAfterConcert() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50) };
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 60; i++) {
+            app.updateQuality();
+        }
+
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-50, app.items[0].sellIn);
+    }
+}
