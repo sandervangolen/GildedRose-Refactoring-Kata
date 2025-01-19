34f742b20a1384cad39d9dd2ac900484374ab6b3
sandervangolen <sander.vangolen@sannister.be>
2025-01-19 19:43:28 +0100

    Refactored AgedBrie to simplify algo.

--- src/main/java/com/gildedrose/AgedBrieInventoryItem.java
+++ src/main/java/com/gildedrose/AgedBrieInventoryItem.java
@@ -7,17 +7,14 @@ public class AgedBrieInventoryItem extends InventoryItem {
 
     @Override
     public void updateQuality() {
-        if (item.quality < 50) {
-            item.quality++;
-        }
-
         item.sellIn--;
 
-        if (item.sellIn < 0) {
-            if (item.quality < 50) {
+        if (item.quality < 50) {
+            item.quality++;
+
+            if (item.sellIn < 0) {
                 item.quality++;
             }
         }
     }
-
 }
