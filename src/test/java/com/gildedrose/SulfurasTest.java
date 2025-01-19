4016fe296aca8b87287f4bfc0962d88e6a988f96
sandervangolen <sander.vangolen@sannister.be>
2025-01-19 14:39:22 +0100

    Added test for Sulfuras.

---
+++ src/test/java/com/gildedrose/SulfurasTest.java
@@ -0,0 +1,18 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SulfurasTest {
+
+    @Test
+    public void sulfurasSellInAndQualityNeverChange() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(20, app.items[0].quality);
+        assertEquals(10, app.items[0].sellIn);
+    }
+}
