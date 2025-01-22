ee8e6d3ccdde9c18ea320504e1f69d6d3b8dd1e4
sandervangolen <sander.vangolen@sannister.be>
2025-01-22 20:21:15 +0100

    Removed unnecessary code.

--- src/main/java/com/gildedrose/InventoryItem.java
+++ src/main/java/com/gildedrose/InventoryItem.java
@@ -1,18 +1,6 @@
 package com.gildedrose;
 
 public interface InventoryItem {
-
-    Item item = null;
-
-    //    public void updateInventory() {
-//        updateQuality();
-//        updateDaysTillExpiration();
-//
-//        if (item.sellIn < 0) {
-//            handleExpired();
-//        }
-//    }
-
     InventoryItem updateQuality();
     InventoryItem updateDaysTillExpiration();
     InventoryItem handleExpired();
