ba1b6f52d3eb4792f97a59c0ead723326b7cbd10
sandervangolen <sander.vangolen@sannister.be>
2025-01-22 19:48:53 +0100

    Changed BackstagePass quality switching to align with requirements.

--- src/main/java/com/gildedrose/BackstagePassesInventoryItem.java
+++ src/main/java/com/gildedrose/BackstagePassesInventoryItem.java
@@ -7,9 +7,9 @@ public class BackstagePassesInventoryItem extends InventoryItem {
 
     @Override
     protected void updateQuality() {
-        if (item.sellIn < 6) {
+        if (item.sellIn <= 5) {
             item.quality = Math.min(item.quality + 3, 50);
-        } else if (item.sellIn < 11) {
+        } else if (item.sellIn <= 10) {
             item.quality = Math.min(item.quality + 2, 50);
         } else {
             item.quality = Math.min(item.quality + 1, 50);
